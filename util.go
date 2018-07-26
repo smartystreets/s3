b98dcf13af4f784cabfc86df3524bbd90e4b9c81
Michael Whatcott <mdwhatcott@gmail.com>
2018-07-26 16:37:57 -0600

    A few convenience functions for dealing with the bucket and key.

--- util.go
+++ util.go
@@ -1,16 +1,28 @@
 package s3
 
 import (
+	"bytes"
+	"io"
 	"net/url"
 	"strings"
 )
+
+// Key ensures the value is suitable for use as an S3 key.
+func Key(value string) string {
+	return strings.Trim(value, "/")
+}
+
+// Content simply wraps blob in a *bytes.Reader.
+func Content(blob []byte) io.ReadSeeker {
+	return bytes.NewReader(blob)
+}
 
 // BucketKey returns the S3 bucket and key embedded in an S3 URL.
 // For details on how s3 urls are formed, please see the S3 docs:
 // https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html
 func BucketKey(address *url.URL) (bucket, key string) {
 	if strings.HasPrefix(address.Host, "s3.") || strings.HasPrefix(address.Host, "s3-") { // path-style
-		path := strings.Trim(address.Path, "/")
+		path := Key(address.Path)
 		elements := strings.Split(path, "/")
 		if len(elements) > 0 {
 			bucket = elements[0]
@@ -20,7 +32,7 @@ func BucketKey(address *url.URL) (bucket, key string) {
 		}
 	} else { // virtual style
 		bucket = strings.Split(address.Host, ".")[0]
-		key = strings.Trim(address.Path, "/")
+		key = Key(address.Path)
 	}
 
 	return bucket, key
